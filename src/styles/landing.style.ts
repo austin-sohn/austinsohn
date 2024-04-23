f3098c2368411b2f96682f169d895cd2375274b8
austinsohn <sohn.austin@gmail.com>
2024-04-23 16:50:49 -0700

    removed some styling

--- src/styles/landing.style.ts
+++ src/styles/landing.style.ts
@@ -1,23 +1,9 @@
 import styled from "styled-components";
 
-const Test = styled.a`
-  scroll-behavior: smooth;
-`;
 const LandingStyle = styled.div`
   margin-top: 55.5px;
   height: 100svh;
-`;
-const CircleStyle = styled.div`
-  width: 330px;
-  height: 330px;
-  border: 1px solid #ccc;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-  border-radius: 50%;
+  scroll-margin-top: 55.5px;
 `;
 
-export { LandingStyle, CircleStyle, Test };
+export default LandingStyle;
