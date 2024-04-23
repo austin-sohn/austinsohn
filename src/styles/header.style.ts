17ca1834ff428c3f9f2761e037a809fd8dc2521b
austinsohn <sohn.austin@gmail.com>
2024-04-22 19:26:33 -0700

    changed header background color

--- src/styles/header.style.ts
+++ src/styles/header.style.ts
@@ -3,11 +3,13 @@ import styled from "styled-components";
 const HeaderStyle = styled.div`
   display: flex;
   position: fixed;
-  background-color: black;
+  background-color: #330033;
   color: white;
   width: 100%;
   height: 55.5px;
   z-index: 1;
+  border-bottom: 1px solid;
+  border-color: lightgrey;
 `;
 const HeaderRight = styled(HeaderStyle)`
   width: 85px;
