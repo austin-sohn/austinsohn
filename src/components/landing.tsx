bc5a2c4462c95bb8e9efd3c16236b0e0b4b8ce08
austinsohn <sohn.austin@gmail.com>
2024-04-23 17:42:16 -0700

    changed scroll method

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,38 +1,33 @@
 // import React from "react";
-import { LandingStyle } from "../styles/landing.style"; // , CircleStyle, Test
+import React from "react";
+import LandingStyle from "../styles/landing.style"; // , CircleStyle, Test
 
-const Landing = () => {
-  return (
-    <LandingStyle id="Landing">
-      <p>
-        Landing Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        eget metus pretium, euismod lectus id, ultrices purus. Donec ut tempus
-        risus, eget tempor nulla. Morbi dolor risus, placerat et pellentesque
-        vitae, eleifend id tortor. Donec id justo maximus, suscipit dui a,
-        ultrices tellus. Fusce eget ullamcorper sem. Nullam iaculis viverra urna
-        ac molestie. Vestibulum vel sagittis lorem.
-      </p>
+class Landing extends React.Component {
+  render() {
+    // some jank way to add offset to window.ScrollTo
+    const yOffset = -55.5;
+    const handleClick = (value: string) => () => {
+      const element = document.getElementById(value)!;
+      const y = element.getBoundingClientRect().top + yOffset;
+      console.log(element);
+      window.scrollTo({ top: y, behavior: "smooth" });
+    };
+    return (
+      <LandingStyle id="Landing">
+        <p>
+          Landing Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+          eget metus pretium, euismod lectus id, ultrices purus. Donec ut tempus
+          risus, eget tempor nulla. Morbi dolor risus, placerat et pellentesque
+          vitae, eleifend id tortor. Donec id justo maximus, suscipit dui a,
+          ultrices tellus. Fusce eget ullamcorper sem. Nullam iaculis viverra
+          urna ac molestie. Vestibulum vel sagittis lorem.
+        </p>
 
-      <button
-        onClick={() => {
-          document
-            .getElementById("Projects")!
-            .scrollIntoView({ behavior: "smooth" });
-        }}
-      >
-        Project
-      </button>
-      <button
-        onClick={() => {
-          document
-            .getElementById("Contact")!
-            .scrollIntoView({ behavior: "smooth" });
-        }}
-      >
-        Contact
-      </button>
-    </LandingStyle>
-  );
-};
+        <button onClick={handleClick("Projects")}>Project</button>
+        <button onClick={handleClick("Contact")}>Contact</button>
+      </LandingStyle>
+    );
+  }
+}
 
 export default Landing;
