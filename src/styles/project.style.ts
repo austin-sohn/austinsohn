ce1b05608d202f3442e155ecc85dede591616f41
austinsohn <sohn.austin@gmail.com>
2024-04-28 22:33:16 -0700

    added different grid formats for smaller screen size

--- src/styles/project.style.ts
+++ src/styles/project.style.ts
@@ -2,14 +2,24 @@ import styled from "styled-components";
 
 const ProjectStyle = styled.div`
   margin-top: 55.5px;
-
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
 `;
 const CardContainer = styled.div`
   display: grid;
-  grid-template-columns: auto auto auto auto;
+  grid-template-columns: repeat(4, 1fr);
+
+  @media screen and (max-width: 1435px) {
+    grid-column-gap: 1vh;
+    grid-row-gap: 1vw;
+  }
+  @media screen and (max-width: 1313px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media screen and (max-width: 660px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
   grid-column-gap: 50px;
   grid-row-gap: 50px;
 `;
