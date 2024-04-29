9bc640ed5e8267d191f8dbc64c55065c748f5335
austinsohn <sohn.austin@gmail.com>
2024-04-28 23:57:05 -0700

    added buttons

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 import React from "react";
-import LandingStyle from "../styles/landing.style"; // , CircleStyle, Test
+import { LandingStyle, ButtonStyle } from "../styles/landing.style"; // , CircleStyle, Test
 
 class Landing extends React.Component {
   render() {
@@ -13,17 +13,16 @@ class Landing extends React.Component {
     };
     return (
       <LandingStyle id="Landing">
-        <p>
-          Landing Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          eget metus pretium, euismod lectus id, ultrices purus. Donec ut tempus
-          risus, eget tempor nulla. Morbi dolor risus, placerat et pellentesque
-          vitae, eleifend id tortor. Donec id justo maximus, suscipit dui a,
-          ultrices tellus. Fusce eget ullamcorper sem. Nullam iaculis viverra
-          urna ac molestie. Vestibulum vel sagittis lorem.
-        </p>
-
-        <button onClick={handleClick("Projects")}>Project</button>
-        <button onClick={handleClick("Contact")}>Contact</button>
+        <ButtonStyle onClick={handleClick("Projects")}>
+          {/* style="--btnClr:#39FF14" */}
+          <span>Projects</span>
+          <i></i>
+        </ButtonStyle>
+        <ButtonStyle onClick={handleClick("Contact")}>
+          {/* style="--btnClr:#0FF0FC" */}
+          <span>Contact</span>
+          <i></i>
+        </ButtonStyle>
       </LandingStyle>
     );
   }
