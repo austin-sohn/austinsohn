d78b16581c57c04fedf68f5b54eb93c76bc4e372
austinsohn <sohn.austin@gmail.com>
2024-04-28 23:59:34 -0700

    started contact section

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,14 +4,7 @@ class Contact extends React.Component<{}> {
   render() {
     return (
       <ContactStyle id="Contact">
-        <p>
-          Contact Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          eget metus pretium, euismod lectus id, ultrices purus. Donec ut tempus
-          risus, eget tempor nulla. Morbi dolor risus, placerat et pellentesque
-          vitae, eleifend id tortor. Donec id justo maximus, suscipit dui a,
-          ultrices tellus. Fusce eget ullamcorper sem. Nullam iaculis viverra
-          urna ac molestie. Vestibulum vel sagittis lorem.
-        </p>
+        <h2>Contact</h2>
       </ContactStyle>
     );
   }
