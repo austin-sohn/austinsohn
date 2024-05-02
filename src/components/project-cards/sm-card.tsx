2a7cf1e7bf936eed5fa8c5dfe0c900c0147d5089
austinsohn <sohn.austin@gmail.com>
2024-05-01 19:54:49 -0700

    changed name of card and the link

---
+++ src/components/project-cards/sm-card.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { CardStyle, ULStyle, H3Style } from "../../styles/cards.style";
+class TwitterCard extends React.Component<{}> {
+  render() {
+    return (
+      <CardStyle
+        href="https://github.com/austin-sohn/Social-Media-Backend"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <H3Style>Social Media Backend</H3Style>
+        <em>AWS, DynamoDB, Foreman, HAProxy, and sqlite-utils</em>
+        <ULStyle>
+          <li>Create an account</li>
+          <li>See own post history or post history of others</li>
+          <li>Like posts</li>
+          <li>Create and vote for polls</li>
+        </ULStyle>
+      </CardStyle>
+    );
+  }
+}
+export default TwitterCard;
